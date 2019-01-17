dbde5f039f332b36e4ef3075600c38ba52fdefe8
Rita <ritazreiby@gmail.com>
2019-01-17 23:35:45 +0100

    Locate an IP through different free IP geolocation services.

---
+++ LocateIP/locateme.py
@@ -0,0 +1,61 @@
+from urllib.request import urlopen
+import json
+import re
+
+def get_IP():
+    IP = str(input("please enter the IP address you want to locate "))
+    return IP
+
+def locateme(IP):
+    # Get Location from ipinfo
+    url = "http://ipinfo.io/" + IP + "/json"
+    response = urlopen(url)
+    data = json.load(response)
+    city = data['city']
+    country = data['country']
+    location = data['loc']
+    org = data['org']
+
+    # Print Extracted Details
+    print("\n")
+    print (f"IP info details for {IP}")
+    print (f"City {city}")
+    print (f"country {country}")
+    print (f"location {location}")
+    print("\n")
+
+
+    #Get location from IP-API
+    url = "http://ip-api.com/json/" + IP
+    response = urlopen(url)
+    data = json.load(response)
+    city = data['city']
+    country = data['country']
+    location = data['lat']
+    asnb = data['as']
+    print (f"IP-API details for {IP}")
+    print (f"City {city}")
+    print (f"country {country}")
+    print (f"location {location}")
+    print (f"AS {asnb}")
+    print("\n")
+
+    #Get location from extreme-IP
+    url = "http://ip-api.com/json/" + IP
+    response = urlopen(url)
+    data = json.load(response)
+    city = data['city']
+    country = data['country']
+    location = data['lat']
+    ispinfo = data['isp']
+    print (f"extreme-IP details for {IP}")
+    print (f"City {city}")
+    print (f"country {country}")
+    print (f"location {location}")
+    print (f"ispinfo {ispinfo}")
+    print("\n")
+
+
+
+IP = get_IP()
+locateme(IP)
